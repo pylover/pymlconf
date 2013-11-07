aa740b3f628e157dc37f1a8d6a25b46f42b40f69
Vahid <vahid_mardani@yahoo.com>
2013-11-07 19:20:35 +0330

    Update __init__.py
    release version without suffix

--- pymlconf/__init__.py
+++ pymlconf/__init__.py
@@ -1,12 +1,11 @@
 
-
 from pymlconf.ConfigNode import ConfigNode
 from pymlconf.ConfigList import ConfigList
 from pymlconf.ConfigDict import ConfigDict
 from pymlconf.ConfigManager import ConfigManager
 from pymlconf.errors import ConfigurationError
 
-__version__ = '0.2.10a'
+__version__ = '0.2.10'
 
 __all__ = ['ConfigManager',
            'ConfigList',
