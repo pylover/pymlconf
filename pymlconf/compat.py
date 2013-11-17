b6c1ea8a53868b11d6c6f5b6469a6cf6d7de8f1a
pylover <vahid.mardani@gmail.com>
2013-11-17 04:47:29 +0330

    compat.py

---
+++ pymlconf/compat.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#@PydevCodeAnalysisIgnore
+'''
+Created on:    Nov 17, 2013
+@author:        vahid
+'''
+
+try:
+    # Python 2.7+
+    from collections import OrderedDict
+except:
+    # Python 2.6-
+    from ordereddict import OrderedDict
+
