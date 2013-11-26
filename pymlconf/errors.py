e3f4e7b10e171d5531d4dca84ddead519240e3d5
vahid <vahid@zahra-ubuntu.(none)>
2013-11-26 23:50:38 +0330

    ConfigurationMergeError

--- pymlconf/errors.py
+++ pymlconf/errors.py
@@ -9,3 +9,7 @@ class ConfigKeyError(ConfigurationError, AttributeError):
     def __init__(self, key):
         AttributeError.__init__(self)
         ConfigurationError.__init__(self,'Config key was not found: "%s"' % key)
+
+class ConfigurationMergeError(ConfigurationError, ValueError):
+    def __init__(self, message):
+        super(ConfigurationError,self).__init__(message)
