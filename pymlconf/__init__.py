b97a9699560a2d28c629bb7871e2af32c9931a66
vahid <vahid@zahra-ubuntu.(none)>
2013-11-26 23:51:32 +0330

    __version__ = '0.3.5'

--- pymlconf/__init__.py
+++ pymlconf/__init__.py
@@ -3,7 +3,7 @@ from pymlconf.config_nodes import ConfigList,ConfigDict
 from pymlconf.config_manager import ConfigManager
 from pymlconf.errors import ConfigurationError
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 __all__ = ['ConfigManager',
            'ConfigList',
