c8ae1fb05d4f6dadf768105308e78e98fd4d4f04
pylover <vahid.mardani@gmail.com>
2015-06-07 21:50:45 +0430

    absolute filepaths in errors

--- pymlconf/errors.py
+++ pymlconf/errors.py
@@ -1,22 +1,28 @@
+from os.path import abspath
+
 # noinspection PyUnresolvedReferences
 class ConfigurationError(Exception):
     def __init__(self, message):
-        super(ConfigurationError,self).__init__(message)
+        super(ConfigurationError, self).__init__(message)
+
 
 class ConfigKeyError(ConfigurationError, AttributeError):
     def __init__(self, key):
         AttributeError.__init__(self)
-        ConfigurationError.__init__(self,'Config key was not found: "%s"' % key)
+        ConfigurationError.__init__(self, 'Config key was not found: "%s"' % key)
+
 
 class ConfigurationMergeError(ConfigurationError, ValueError):
     def __init__(self, message):
-        super(ConfigurationError,self).__init__(message)
+        super(ConfigurationError, self).__init__(message)
+
 
 class ConfigFileNotFoundError(ConfigurationError):
     def __init__(self, filename):
-        ConfigurationError.__init__(self,'Config File not found: "%s"' % filename)
+        ConfigurationError.__init__(self, 'Config File not found: "%s"' % abspath(filename))
+
 
 class ConfigFileSyntaxError(ConfigurationError):
     def __init__(self, filename, inner_exception):
         ConfigurationError.__init__(self, '%s\nFilename: %s"'
-                                    % (str(inner_exception), filename))
+                                    % (str(inner_exception), abspath(filename)))
