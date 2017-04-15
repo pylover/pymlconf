97c849980983e60dab63e81f6657f5970dfe5944
pylover <vahid.mardani@gmail.com>
2017-04-16 00:24:51 +0430

    Coverals, lemurheavy/coveralls-public#487

--- pymlconf/__init__.py
+++ pymlconf/__init__.py
@@ -5,7 +5,7 @@ from pymlconf.errors import ConfigurationError, ConfigKeyError, ConfigurationMer
     ConfigurationAlreadyInitializedError, ConfigurationNotInitializedError
 from pymlconf.proxy import DeferredConfigManager
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 __all__ = ['ConfigManager',
            'Mergable',
