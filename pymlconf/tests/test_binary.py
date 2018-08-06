fc7462555f0369b26b57f51453e6abed478c3e20
mohammad <mohammadsheikhian70@gmail.com>
2018-08-06 19:10:22 +0430

    Bug report in binary configuration

---
+++ pymlconf/tests/test_binary.py
@@ -0,0 +1,18 @@
+import unittest
+
+from pymlconf import ConfigManager
+
+
+class TestBinary(unittest.TestCase):
+
+    def test_binary(self):
+        config = '''
+            app:
+              name: MyApp
+            secret: !!binary YWJj\n
+        '''
+        cm = ConfigManager(init_value=config)
+
+        self.assertEqual(cm.app.name, 'MyApp')
+        self.assertEqual(cm.secret, b'abc')
+
