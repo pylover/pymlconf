4c3870babc4338586a0cfc6288741c2b41c32665
pylover <vahid.mardani@gmail.com>
2018-08-06 22:55:13 +0430

    v0.8.8

--- pymlconf/__init__.py
+++ pymlconf/__init__.py
@@ -6,7 +6,7 @@ from pymlconf.errors import ConfigurationError, ConfigKeyError, ConfigurationMer
 from pymlconf.proxy import DeferredConfigManager
 
 
-__version__ = '0.8.7'
+__version__ = '0.8.8'
 
 
 __all__ = ['ConfigManager',
