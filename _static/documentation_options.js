87424a1eecdc3e2f4d59618785d710c8ba689202
Travis CI <vahid@dobisel.com>
2018-08-09 23:17:36 +0000

    Deploy to GitHub Pages: e69fc670cf63707379e7a76bfeed9bbcd4b979ab

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.9',
-    LANGUAGE: 'html',
+    VERSION: '1.0.0a0',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
