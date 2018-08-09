011ab21469aa0863c718eec9edf4c4f63a5906f3
pylover <vahid.mardani@gmail.com>
2018-08-10 00:44:39 +0430

    test_root has been passed, #31, #33

---
+++ pymlconf/tests/test_root.py
@@ -0,0 +1,50 @@
+import re
+import unittest
+from os import path
+
+from pymlconf import MergableDict, MergableList, Root
+
+
+class TestConfigManager(unittest.TestCase):
+    def test_constructor(self):
+        context = dict(
+            c=3
+        )
+
+        builtin = '''
+          a:
+            a1: 1
+
+          b:
+          - 1
+          - 2
+          - %(c)s
+        '''
+
+        root = Root(builtin, context)
+        self.assertEqual(root.a.a1, 1)
+        self.assertEqual(root.b[0], 1)
+        self.assertEqual(root.b[1], 2)
+        self.assertEqual(root.b[2], 3)
+
+        root.merge('''
+          a:
+            a2: 2
+        ''')
+        self.assertEqual(root.a.a2, 2)
+
+        root.a.a3 = 3
+        self.assertEqual(root.a.a1, 1)
+        self.assertEqual(root.a.a2, 2)
+        self.assertEqual(root.a.a3, 3)
+
+        root.merge('''
+          b:
+            - 4
+        ''')
+        self.assertEqual(root.b[0], 4)
+        self.assertEqual(1, len(root.b))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
