085ef5121a3efbd9abee60a91f9e74f3f92701cf
pylover <vahid.mardani@gmail.com>
2026-05-02 22:22:27 +0400

    Fix: Github actions

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -10,22 +10,13 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 html_theme = 'python_docs_theme'
 
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
-
-# The master toctree document.
-master_doc = 'index'
 
 # General information about the project.
 project = 'pymlconf'
